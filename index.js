6ec338c38c131632372ba6c494591603dc617636
Taboichebet <taboibrenda@gmail.com>
2022-08-23 08:34:05 +0300

    assignment completed

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
+
+
 const titleCased = () => {
-  return tutorials
+
+     let map1 =tutorials.map((items)=>{
+
+    let words =items.split(" ");
+    for(let i=0;i<words.length;i++){
+       words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+    }
+   return  words.join(" ")
+
+
+});
+
+  return map1
 }
